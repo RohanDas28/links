e258d3b4b5e4eaefa8d2459f748f098445fcdd96
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2025-07-06 14:12:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/links/",
+export default defineConfig(() => ({
+  base: "/links/", // make sure your repo name is 'links'
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false, // Optional
   },
 }));
